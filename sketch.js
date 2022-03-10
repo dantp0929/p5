9c42f0da09c6a4926c530c05ebfece8ba5608faa
Cole D. Connelly <cdconn00@gmail.com>
2022-03-09 18:45:38 -0600

    Add upvote.

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ let homeButton, searchbutton;
 let homePage;
 let notificationButton;
 let searchImg;
+let upvoteImg;
+let swiperButton;
 
 function preload() {
   homePage = loadImage('assets/HomePage.png');
   searchImg = loadImage('assets/Search.png');
+  upvoteImg = loadImage('assets/Upvote.png');
 }
 
 function setup() {
@@ -16,6 +19,8 @@ function setup() {
 
   setupSearchButton();
   setupNotificationButton();
+  setupCanvasSwipe();
+  setupCanvas2Swipe();
 }
 
 function setupSearchButton() {
@@ -40,3 +45,20 @@ function setupNotificationButton() {
   notificationButton.style('border', 'none');
   notificationButton.style('cursor', 'pointer');
 }
+
+function setupCanvasSwipe() {
+  swiperbutton = createButton('');
+  swiperbutton.position(25, 100);
+  swiperbutton.size(250, 100);
+  swiperbutton.style('background-color', 'transparent');
+  swiperbutton.style('border', 'none');
+  swiperbutton.style('cursor', 'pointer');
+  swiperbutton.mousePressed(showUpvote);
+}
+
+function showUpvote() {
+  let newImg = image(upvoteImg, 25, 100);
+  swiperbutton.mousePressed(showHomeMenu);
+}
+
+function setupCanvas2Swipe() {}
